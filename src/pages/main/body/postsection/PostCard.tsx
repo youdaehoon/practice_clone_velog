14cfadc131e509334ebecca9ca2c883f718d274b
Dae Hoon YOU <sjssmsqkqh1@gmail.com>
2022-12-06 17:18:42 +0900

    feat: main-body 작업중
    - 코드 리뷰를 위한 커밋입니다
    
    related #1

--- src/pages/main/body/postsection/PostCard.tsx
+++ src/pages/main/body/postsection/PostCard.tsx
@@ -3,37 +3,58 @@ import ProfileImage from '../../../../components/ProfileImage'
 import Card from '../../../../components/ui/Card'
 import MyImage from '../../../../components/ui/MyImage'
 
+import heart from"../../../../assets/heart.png"
+
+import "./postcard.css"
+import "../../../../styles/common.css"
+
 interface Iprops{
-
+data:{
+    url:string,
+    title:string,
+    content:string,
+    date:string,
+    numReply:number,
+    user:{
+        profile:string,
+        nick:string
+},
+numLike:number
 }
-const PostCard:React.FC<Iprops> = () => {
-    const data={
-        url:'https://velog.velcdn.com/images/ninto_2/post/e4eaa7ec-223f-4ada-be19-11dadf1d8d0f/image.gif',
-        title:"✍커밋 컨벤션",
-        content:`"커밋 메세지 쓸 때도 컨벤션이 존재했다고..?😨"`,
-        date:"2012년 11월 27일",
-        numReply:4,
-        user:{
-            profile:"https://velog.velcdn.com/images/ninto_2/profile/803bef73-6b7d-4af8-8592-ee361c8b9e61/image.jpg",
-            nick:"ninto_2"
-    },
-    numLike:125
-
-    }
+}
+const PostCard:React.FC<Iprops> = ({data:data}) => {
+       
   return (
-    <Card width={320} height={377.013} borderRadius={4}>
-        <>
+    <div className=''>
+        <Card width={20} height={23.5} borderRadius={4}>
+        <div className='flex flex-column space-between'>
         <MyImage url={data.url} height={167.012}></MyImage>
-        <div>
-        <h4>{data.title}</h4>
-        <p>{data.content}</p>
-        <div><span>{data.date}</span> <span>{data.numReply}</span>개의 댓글</div>
+        
+        <div className='wrap-post-body flex flex-column space-between'>
+            <a>
+                <h4 className='title'>{data.title}</h4>
+                <p className='desc'>{data.content}</p>
+            </a>
+        <div className='post-info gray'> <span>{data.date}</span> <span>{data.numReply}</span>개의 댓글</div>
         </div>
-        <div>
-            <ProfileImage url={data.user.profile} width={24} height={24}/> {data.user.nick} {data.numLike}</div>
-        </>
+        <div className='footer flex  space-between align-center'>
+           <div className='flex align-center gap'>
+             <ProfileImage url={data.user.profile} width={24} height={24}/>
+             <span className='gray'>by</span>
+             <span>{data.user.nick}</span>  
+            </div>
+            <div className='flex align-center gap'>
+            <img src={heart} width={24} height={24} ></img>
+            <span>  {data.numLike}</span>
+            </div>
+          
+        </div> 
+        </div>
+            
         
     </Card>
+    </div>
+   
   )
 }
 
