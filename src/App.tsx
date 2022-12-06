af517894eedb695a74bdf13405d99fb806934b1e
hahbr88 <hahbr88@hanmail.net>
2022-12-06 16:59:16 +0900

    feat: 프로필 사진 제외한 헤더 부분, style: common.css , header.css 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
-import React, { useState } from 'react';
-import {Route,Routes} from"react-router-dom"
-import Header from './components/ui/Header';
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/ui/Header";
 
-import Detail from './pages/detail';
-import Main from './pages/main';
-import Write from './pages/write';
-
-export interface IProps {
-  darkmode: boolean;
-}
+import Detail from "./pages/detail";
+import Main from "./pages/main";
+import Write from "./pages/write";
 
 
-const App:React.FC = () => {
 
-  const [darkMode, setDarkMode] = useState<IProps>(true);
+const App: React.FC = () => {
+  const [darkMode, setDarkMode] = useState<boolean>(true);
+
   return (
-    <div className='app'>
-    <Header darkMode={darkMode} setDarkMode={setDarkMode}  />
-    <Routes>
-      <Route path='/' element={<Main/>}/>
-      <Route path='/detail' element={<Detail/>}/>
-      <Route path='/write' element={<Write/>}/>
-    </Routes>
+    <div className="app">
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/write" element={<Write />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,33 +2,64 @@ import React, { useState } from "react";
 import "../../styles/common.css";
 import "../../styles/header.css";
 import { BsSun, BsMoon, BsSearch } from "react-icons/bs";
+import { GoTriangleDown } from "react-icons/go";
+interface HeaderProps {
+  darkMode: boolean;
+  setDarkMode: any;
+}
 
-const Header: React.FC = ({ darkMode, setDarkMode }) => {
-  
+const Header: React.FC<HeaderProps> = ({ darkMode, setDarkMode }) => {
+  const [toggleBar, setToggleBar] = useState<boolean>(false);
+
+  const toggleMode = (state: boolean, setFunc: any) => setFunc(!state);
 
   return (
-    <div className="header-wrap flex-sb">
-      <div className="logo flex-center"></div>
+    <div className=" flex-center main-background-color">
+      <div className="header-wrap flex-sb">
+        <div className="logo flex-center"></div>
 
-      <div className="nav-wrap flex">
-        {darkMode ? (
-          <div>
-            <BsSun onClick={() => setDarkMode(!darkMode)} />
+        <div className="nav-wrap flex">
+          {darkMode ? (
+            <div
+              className="nav-icon flex-center"
+              onClick={() => toggleMode(darkMode, setDarkMode)}
+            >
+              <BsSun size={30} />
+            </div>
+          ) : (
+            <div
+              className="nav-icon flex-center"
+              onClick={() => toggleMode(darkMode, setDarkMode)}
+            >
+              <BsMoon size={30} />
+            </div>
+          )}
+
+          <div className="nav-icon flex-center">
+            <BsSearch size={25} />
           </div>
-        ) : (
-          <div>
-            <BsMoon onClick={() => setDarkMode(!darkMode)} />
+          <div className="new-article flex-center">새글 작성</div>
+          <div className="profile-wrap flex-sb">
+            <div>프사</div>
+            <div
+              className="toggle_btn flex-center"
+              onClick={() => toggleMode(toggleBar, setToggleBar)}
+            >
+              <GoTriangleDown />
+            </div>
+          </div>
+        </div>
+        {toggleBar && (
+          <div className="bars">
+            <ul>
+              <li>내 벨로그</li>
+              <li>임시 글</li>
+              <li>읽기 목록</li>
+              <li>설정</li>
+              <li>로그아웃</li>
+            </ul>
           </div>
         )}
-
-        <div>
-          <BsSearch />
-        </div>
-        <div>새글작성</div>
-        <div>
-          <div>프사</div>
-          <div>토글</div>
-        </div>
       </div>
     </div>
   );
