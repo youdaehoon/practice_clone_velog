8ffff88c53375f6ef46a9317b5e4c31f809ff840
Dae Hoon YOU <sjssmsqkqh1@gmail.com>
2022-12-07 11:45:29 +0900

    create: layout
    - padding 값을 따로 주지 않는다면 좌우 1rem이 default 입니다
    - classname을 array로 전달하여 common.css를 적용할 수 있습니다.
    
    related: #6

---
+++ src/components/ui/Layout.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import "../../styles/common.css"
+
+interface IProps{
+children:React.ReactElement
+padding?:string|"0rem 1rem 0rem 1rem"
+classNameArrCommon?:Array<string>
+
+}
+
+const Layout :React.FC<IProps> = ({children:c, padding:p,classNameArrCommon:ca}) => {
+    let className=""
+    if(ca){
+        className=ca.join(" ")
+    }
+
+    p=p||"0rem 1rem 0rem 1rem";
+    
+    const style={
+        padding:p,
+        width:"100%"
+    } 
+  return (
+    <div className={className} style={style}>{c}</div>
+  )
+}
+
+export default Layout
