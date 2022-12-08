abfc4b08a6928bade889f101af93115e5293c05d
Dae Hoon YOU <sjssmsqkqh1@gmail.com>
2022-12-08 14:46:16 +0900

    feat: detail/layout 적용

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Layout from '../../components/ui/Layout'
 import TitleSection from './body/titlesection'
 
 const Detail:React.FC = () => {
   return (
-    <>
+    <Layout>
     <TitleSection></TitleSection>
-    </>
+    </Layout>
   )
 }
 
